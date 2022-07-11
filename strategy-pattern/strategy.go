f591bd26520ccd921ec65feaf804853d6f997007
p1nant0m <wgblike@outlook.com>
2022-07-11 14:20:07 +0800

    add: adding comments

--- strategy-pattern/strategy.go
+++ strategy-pattern/strategy.go
@@ -8,12 +8,14 @@ type IStrategy interface {
 
 type add struct{}
 
+// do implement the IStrategy interface and do addition calculation
 func (*add) do(a, b int) int {
 	return a + b
 }
 
 type reduce struct{}
 
+// do implement the IStrategy interface and do reduction calculation
 func (*reduce) do(a, b int) int {
 	return a - b
 }
@@ -22,10 +24,13 @@ type Operator struct {
 	strategy IStrategy
 }
 
+// setStrategy set the field "strategy" in operator instance
 func (operator *Operator) setStrategy(strategy IStrategy) {
 	operator.strategy = strategy
 }
 
+// calculate do the specific calculation based on different implementation
+// of strategy and return the result of that calculation
 func (operator *Operator) calculate(a, b int) int {
 	return operator.strategy.do(a, b)
 }
